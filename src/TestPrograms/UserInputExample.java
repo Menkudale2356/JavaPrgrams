387b2cc480381a8b3272acc852aff4be81ba7116
Menkudale2356 <om2314@gmail.com>
2026-01-04 15:15:52 +0530

    First commit 4Dec

--- src/TestPrograms/UserInputExample.java
+++ src/TestPrograms/UserInputExample.java
@@ -1,2 +1,22 @@
-package TestPrograms;public class UserInputExample {
+package TestPrograms;
+
+import java.util.Scanner;
+
+public class UserInputExample {
+    public static void main (String[] args){
+        // Create a scanner object to read user input
+        Scanner scanner=new Scanner(System.in);
+        //Prompt the user for input
+        System.out.print("Enter your name: ");
+        //Read a line of text entered by the user
+        String name = scanner.nextLine();
+        //Prompt the user for a number
+        System.out.print("Enter your age: ");
+        //Read an integer entered by the user
+        int age=scanner.nextInt();
+        //Display the user's input
+        System.out.println("Hello, "+ name+ "! You are " + age+ "years old.");
+        scanner.close();
+    }
 }
+
